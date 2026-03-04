856f61a7e5ac96bed85a7fa8e75436bde386f5b3
Hoang Dieu <dieubh234112e@st.uel.edu.vn>
2026-03-04 23:16:50 +0700

    fix: api url

--- room-bookingFE/src/environments/environment.ts
+++ room-bookingFE/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiBaseUrl: 'http://localhost:5000/api',
-  apiBaseUrl:'https://room-booking-9zg2.onrender.com'
+  // apiBaseUrl: 'http://localhost:5000/api',
+  apiBaseUrl: 'https://room-booking-9zg2.onrender.com/api',
 };
